eedf025867bcdb272a6e86eab38c4693585ad521
Zhi Wang <zwang@cs.fsu.edu>
2022-01-04 15:41:41 -0500

    Initial checkin

---
+++ assets/README.md
@@ -0,0 +1,7 @@
+This directory contains xterm.js 4.16
+
+To update this to a newer version of xterm.js, run 
+`npm install xterm.js` in a different directory, 
+and copy xterm.js and xterm.js.map and xterm.css here. 
+
+Do the same to three of xterm.js's addons. 
---
+++ main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"net/http"
+	"net/url"
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+)
+
+var host *string = nil
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize : 4096,
+	WriteBufferSize: 4096,
+	CheckOrigin: func(r *http.Request) bool {
+		org := r.Header.Get("Origin")
+		h,err := url.Parse(org)
+
+		if err != nil {
+			return false
+		}
+
+		if (host == nil) || (*host != h.Host) {
+			fmt.Println("failed origin check of ", org, "against", *host)
+		}
+
+		return (host != nil) && (*host == h.Host)
+	},
+}
+
+// handle websockets
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w,r, nil)
+
+	if err!= nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Created the websocket")
+
+	for {
+		msgType, p, err := conn.ReadMessage()
+
+		if err!= nil {
+			fmt.Println(err)
+			return
+		}
+
+		if err := conn.WriteMessage(msgType, p); err!= nil {
+			fmt.Println(err)
+			return
+		}
+	}
+}
+
+// return files
+func fileHandler (c *gin.Context, fname string) {
+	// if the URL has no fname, c.Param returns "/"
+	if fname == "/" {
+		fname = "/index.html"
+		host = &c.Request.Host
+	}
+
+	fname = fname[1:] //fname always starts with /
+	fmt.Println(fname)
+
+	if strings.HasSuffix(fname, "html") {
+		c.HTML(200, fname, nil)
+	} else {
+		//c.HTML interprets the file as HTML file
+		//we do not need that for regular files
+		c.File(fmt.Sprint("assets/", fname))
+	}
+}
+
+func main () {
+	rt := gin.Default()
+
+	rt.SetTrustedProxies(nil)
+	rt.LoadHTMLGlob("assets/*.html")
+
+	rt.GET("/*fname", func(c *gin.Context){
+		fname := c.Param("fname")
+
+		// ws is a special case to create a new websocket
+		switch fname {
+		case "/ws": 
+			wsHandler(c.Writer, c.Request)
+		default:
+			fileHandler(c, fname)
+		}
+	})
+
+	rt.Run(":8080")
+}
